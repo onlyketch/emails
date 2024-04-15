61ebd2fe0bda2a93028304d23381dd08ed0fc61b
Andrew <byketch@yandex.ru>
2024-04-15 17:03:42 +0300

    remove add

--- jquery-ui.js
+++ jquery-ui.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 	//Test
-	console.log('this is test message');
+	console.log('this is test message'); 
 
-
-
+	let elem = document.querySelector('.qwerty6');
+	elem.remove();
 
 });
