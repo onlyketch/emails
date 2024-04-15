9a151c629d113905064222eb9d6e9ef3534fbf7f
Andrew <byketch@yandex.ru>
2024-04-15 17:11:16 +0300

    rem 2

--- jquery-ui.js
+++ jquery-ui.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	//Test
 	console.log('this is test message'); 
 
-	let elem = document.querySelector('.qwerty6');
+	let elem = document.getElementById('qwerty6');
 	elem.remove();
 
 });
